9e43cabcc1ef835ff8c8346062fead1b23707454
Steve Flores <stevewflores43@gmail.com>
2024-01-24 23:30:20 -0800

    ???

--- src/app/board-hub/BoardHub.tsx
+++ src/app/board-hub/BoardHub.tsx
@@ -10,10 +10,10 @@ const BoardHub = () => {
     //API call will populate this later on
     const [myBoards, setMyBoards] = useState([]);
     const overlayRef = useRef<any>(null)
-    const {isVisible : showOverlay,setIsVisible: setShowOverlay} = useOutSideClick(overlayRef);    
+    const { isVisible: showOverlay, setIsVisible: setShowOverlay } = useOutSideClick(overlayRef);
     const tempBoard = {
         title: 'skibiddy toilet',
-        date : '02/28/2024'
+        date: '02/28/2024'
     }
 
     return (
@@ -21,7 +21,7 @@ const BoardHub = () => {
             <div className={`${showOverlay ? "visible" : "invisible"} absolute flex flex-col z-10 mx-auto justify-center w-full h-2/3 items-center`}>
                 <div className='flex relative flex-col gap-y-4 opacity-95 bg-gray-700 p-8 rounded-md text-white max-w-sm min-w-[20rem]' ref={overlayRef}>
                     <div className='absolute right-4 top-4 rounded-full hover:bg-slate-950 hover:cursor-pointer p-1'>
-                        <BsX  onClick={() => setShowOverlay(false)} />
+                        <BsX onClick={() => setShowOverlay(false)} />
                     </div>
                     <h1 className='text-2xl'>Create New Board</h1>
                     <InputSecondary placeholder="Title"></InputSecondary>
@@ -32,24 +32,24 @@ const BoardHub = () => {
                 <button className='bg-secondary p-2 flex flex-row items-center gap-1 rounded-md hover:bg-third text-white font-semibold mb-2' onClick={() => setShowOverlay(!showOverlay)}>New Board <BsPlus /></button>
                 <div className='flex gap-2 flex-wrap justify-evenly'>
                     {/* temporary stuff that'll be populated later */}
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
-                    <BoardCard board={tempBoard}/>
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
+                    <BoardCard board={tempBoard} />
                 </div>
             </div>
         </>
--- src/app/board-hub/create/Create.tsx
+++ src/app/board-hub/create/Create.tsx
@@ -4,11 +4,13 @@ import BoardEntry from '@/components/boardEntry/BoardEntry'
 import React, { useEffect, useRef, useState } from 'react'
 import { defaultBoard } from './util'
 import { PrimaryButton } from '@/components/Buttons'
-
+import './Create.css'
+import { BsDash, BsPatchMinus, BsPlus } from 'react-icons/bs'
 
 const Create = () => {
-    const [cols, setCols] = useState(5)
+    const [cols, setCols] = useState<number>(5)
     const [cards, setCards] = useState<React.ReactNode[]>([])
+    const [numOfBoards, setnumOfBoards] = useState<number>(2)
 
     useEffect(() => {
 
@@ -30,15 +32,53 @@ const Create = () => {
     const editCard = () => {
 
     }
+    const removeBoard = () => {
+        if (numOfBoards > 0) {
+            setnumOfBoards(numOfBoards - 1)
+        }
+    }
+    const addBoard = () => {
+        if (numOfBoards < 3) {
+            setnumOfBoards(numOfBoards + 1)
+        }
+    }
+
+    const displayBoards = () => {
+        const elems = []
+        for (let i = 0; i < numOfBoards; i++) {
+            elems.push(
+                <button type="button" className="button-group border-t border-b" id={'board-' + i} key={i}>
+                    Board {i + 1}
+                </button>
+            )
+        }
+        return elems
+    }
 
     return (
-        <div className='p-12 flex justify-center sm:block transition-all duration-100'>
-            <div>
-                <h1>TITLE</h1>
-                <PrimaryButton>Save?</PrimaryButton>
+        <div className='p-12 flex flex-col items-center sm:block transition-all duration-100'>
+            <div className='flex flex-col items-center gap-y-8'>
+                <h1 className='text-3xl font-bold'>TITLE</h1>
+                <div className='flex gap-8 flex-wrap justify-center'>
+                    <PrimaryButton>Save?</PrimaryButton>
+                    <div className="inline-flex rounded-md shadow-sm" role="group">
+
+                        <button type="button" className="button-group rounded-s-md" onClick={removeBoard}>
+                            <BsDash className='text-lg' />
+                        </button>
+                        {displayBoards()}
+                        <button type="button" className="button-group rounded-e-md" onClick={addBoard}>
+                            <BsPlus className='text-lg' />
+                        </button>
+                    </div>
+
+                </div>
             </div>
-            <div className={`grid grid-cols-5 p-4 max-w-screen-xl mx-auto min-w-[630px] scale-50 sm:scale-100 transition`}>
-                {cards}
+            <div className='relative'>
+                <div className='absolute w-full h-full mx-auto bg-third z-10 text-white opacity-95 invisible'> yup cock</div>
+                <div className={`grid grid-cols-${cols} p-4 max-w-screen-xl min-w-[600px] scale-50 sm:mx-auto sm:scale-100 transition`}>
+                    {cards}
+                </div>
             </div>
         </div>
     )
